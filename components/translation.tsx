b6c9a150aebd1501374eb699bb6c4cf77caf556f
Animesh Pathak <kurosakiichigo.songoku@gmail.com>
2024-11-29 12:56:03 +0530

    Revert "Revert "feat: UI and API""
    This reverts commit b1f42f1917a7b565fe5d828d2353f95346c2f8e2.

--- components/translation.tsx
+++ components/translation.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React, { useState } from 'react'
-import { Languages, ChevronDown, Loader2, RefreshCcw } from 'lucide-react'
+import { Languages, Loader2, RefreshCcw } from 'lucide-react'
 import { Button } from './ui/button'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './ui/select'
 import { Textarea } from './ui/textarea'
